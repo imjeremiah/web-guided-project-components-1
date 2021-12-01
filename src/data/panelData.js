66fb79318f721805aee535da2b2757bb884d7e3f
Casey Harding <casey.r.harding@gmail.com>
2021-12-01 12:40:56 -0500

    Through task 5.

--- src/data/panelData.js
+++ src/data/panelData.js
@@ -17,4 +17,4 @@
   },
 ]
 
-export default panelData
+export default panelData;
--- src/index.js
+++ src/index.js
@@ -51,7 +51,6 @@ const accordion = document.querySelector('.accordion');
 
 // TASK 4- Create a function 'makePanel' that creates a panel exactly as you see it in the HTML.
 function makePanel(obj) {
-  console.log(obj);
   // <!-- Remove this -->
   // <div class="panel">
   //   <div class="panel-bar">
@@ -69,13 +68,13 @@ function makePanel(obj) {
 
 
   // TASK 5- Instantiate all the elements needed for a panel
-  const panel = null
-  const panelBar = null
-  const panelContent = null
-  const panelTitle = null
-  const panelButtons = null
-  const openButton = null
-  const closeButton = null
+  const panel = document.createElement('div');
+  const panelBar = document.createElement('div');
+  const panelContent = document.createElement('div');
+  const panelTitle = document.createElement('h3');
+  const panelButtons = document.createElement('div');
+  const openButton = document.createElement('button');
+  const closeButton = document.createElement('button');
 
 
   // TASK 6- Setup the structure of our elements
